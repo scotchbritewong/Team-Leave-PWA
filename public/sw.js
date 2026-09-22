516b6b1fc25ecc7a0c505a2ebc08302523f9e53c
scotchbritewong <epitaph.m@gmail.com>
2026-09-22 10:17:11 +0800

    Add service worker with install and activate events

---
+++ public/sw.js
@@ -0,0 +1,25 @@
+self.addEventListener("install", () => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    Promise.all([
+      self.registration.unregister(),
+
+      caches.keys().then((cacheNames) =>
+        Promise.all(
+          cacheNames.map((cacheName) =>
+            caches.delete(cacheName)
+          )
+        )
+      ),
+
+      self.clients.matchAll().then((clients) => {
+        clients.forEach((client) => {
+          client.navigate(client.url);
+        });
+      })
+    ])
+  );
+});
